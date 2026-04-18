17f727d461c573ca6c32c15b532a97f14bdd19f5
Aditya Rao <adityaraousa05@gmail.com>
2026-04-18 15:31:21 +1000

    Added imperial bmi calculation

--- app.py
+++ app.py
@@ -3,29 +3,76 @@ import streamlit as st
 st.title("BMI Calculator")
 st.subheader("For your BMI needs")
 
-height = st.number_input("Enter your height in centimeters: ", min_value = 0.1, step = 0.1)
-#height_slider = st.slider("Pick a value between 0 and 250 cms")
+unit_select = st.radio("Choose your units: ", ["Metric", "Imperial"]) # button to select imperial or metric
 
-weight = st.number_input("Enter your weight in kilograms: ", min_value = 0.1, step = 0.1)
-#weight_slider = st.slider("Pick a weight between 0 and 250 kgs")
+
+if unit_select == "Metric":
+    
+    height = st.number_input("Enter your height in centimeters: ", min_value = 0.1, step = 0.1)
+    #height_slider = st.slider("Pick a value between 0 and 250 cms")
+
+    weight = st.number_input("Enter your weight in kilograms: ", min_value = 0.1, step = 0.1)
+    #weight_slider = st.slider("Pick a weight between 0 and 250 kgs")
+    
+else:
+    
+    height_imp = st.number_input("Enter your height in inches: ", min_value = 0.1, step = 0.1)
+    weight_imp = st.number_input("Enter your weight in lbs: ", min_value = 0.1, step = 0.1)
+
+
+
+    
+
+# metric calculation
 
 
 if st.button("Calculate BMI"):
-    height_cms = height/100
-    bmi = weight/ (height_cms**2)
     
-    st.write(f"Your bmi is: {bmi:.1f}")
-
-    if bmi < 18.5:
-        st.warning("You are underweight")
+    if unit_select == "Metric":
+    
+        height_cms = height/100
+        bmi = weight / (height_cms**2)
         
-    elif bmi > 18.5 and bmi <= 24.9:
-        st.success("You are a healthy weight")
-        
-    elif bmi >= 25 and bmi <= 29.9:
-        st.info("You are overweight")
+        st.write(f"Your bmi is: {bmi:.1f}")
 
-    else:
-        st.info("You are obese")
+        if bmi < 18.5:
+            st.warning("You are underweight")
+            
+        elif bmi < 25:
+            st.success("You are a healthy weight")
+            
+        elif bmi < 30:
+            st.info("You are overweight")
+
+        else:
+            st.info("You are obese")
+            
+    elif unit_select == "Imperial":  # imperial 
+        
+
+        height_isq = height_imp**2
+        bmi = weight_imp / (height_isq)*703
+
+        st.write(f"Your bmi is {bmi:.1f}")
+        
+        if bmi < 18.5:
+            st.warning("You are underweight")
+            
+        elif bmi < 25:
+            st.success("You are a healthy weight")
+            
+        elif bmi < 30:
+            st.info("You are overweight")
+
+        else:
+            st.info("You are obese")
+
+        
+        
+
+        
+
+    
+    
         
         
